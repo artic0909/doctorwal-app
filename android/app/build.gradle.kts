26f6f9e85065f2aab7c37d4aa7173d74c3bf795a
artic0909 <alfag327@gmail.com>
2026-04-08 14:46:23 +0530

    ttt

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ val keystoreProperties = Properties().apply {
 android {
     namespace = "com.example.demoapp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
